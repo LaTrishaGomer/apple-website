24ac97953f128bb8a10a2aa88cce1eeddac8a919
latrisha <l_gomer@yahoo.com>
2025-09-01 16:26:39 -0700

    wip: create hero, highlights, navbar components and constants

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { appleImg } from "../utils";
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src={appleImg} alt="Apple" width={14} height={18} />
+                <div>
+                    {['Phones', 'Macbooks', 'Tablets'].map((nav,
+                    i) => (
+                    <div>
+                        {nav}
+                    </div>
+                 ))}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
