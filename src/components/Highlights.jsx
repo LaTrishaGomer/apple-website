a415b5f9198cbe31c37bf15113b613e0b854dfdd
latrisha <l_gomer@yahoo.com>
2025-09-03 14:35:00 -0700

    feat: Setup custom video carousel component for highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
 
 const Highlights = () => {
     useGSAP(() => {
@@ -30,8 +31,10 @@ const Highlights = () => {
                         className="ml-2" />
                         </p>
                     </div>
-
                 </div>
+
+                <VideoCarousel />
+
             </div>
         </section>
     )
